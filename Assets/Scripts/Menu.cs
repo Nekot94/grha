67036f501067ef63cdac224c4a5d7c434f2ab347
mrrrlisn <mrrrrlisin@gmail.com>
2018-06-08 14:14:35 +0300

    menu+vr1

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    /// <summary>
+    /// Меняет уровень по имени
+    /// </summary>
+    /// <param name="name"> имя уровня</param>
+    public void ChangeLevel(string name)
+    {
+        SceneManager.LoadScene(name); 
+    }
+
+    /// <summary>
+    /// выход приложения из билда
+    /// </summary>
+    public void Exit()
+    {
+        Application.Quit(); 
+    }
+
+}
---
+++ Assets/Scripts/VRController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class VRController : MonoBehaviour
+{
+    public Transform cam;
+    public float speed = 3;
+    public float toggleAngle = 20;
+
+    Rigidbody body;
+
+    bool isMoving;
+
+
+    void Start ()
+    {
+        body = GetComponent<Rigidbody>();
+	}
+	
+	void FixedUpdate ()
+    {
+        isMoving = cam.eulerAngles.x >= toggleAngle && cam.eulerAngles.x < 90;
+        //isMoving = true;
+        if (isMoving)
+        {
+            Vector3 forward = cam.TransformDirection(Vector3.forward);
+            body.velocity = forward * speed;
+        }
+	}
+}
